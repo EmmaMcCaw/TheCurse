1f8983112156a007e566ca3bde0ef8c10a9541b7
Emma McCaw <1020368@student.sae.edu.au>
2021-07-16 16:30:45 +1000

    Village Art Scene completed

---
+++ Assets/TestPM.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPM : MonoBehaviour
+{
+    public float Speed = 20f;
+    float horizontal;
+   // float vertical;
+    public Rigidbody2D playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        //vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //playerController.velocity = new Vector(horizontal * Speed, vertical * Speed);
+    }
+
+}
